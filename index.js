e9d1d8b4415f6319548491a5e22d6bd1e6bbf7c4
BaoNQ <nqbao2712@gmail.com>
2023-12-07 08:56:58 +0700

    add APIs

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const accountRoutes = require("./routes/account.routes");
+const novelRoutes = require("./routes/novel.routes");
+const chapterRoutes = require("./routes/chapter.routes");
+const bookmarkRoutes = require("./routes/bookmark.routes");
+const reviewRoutes = require("./routes/review.routes");
+const commentRoutes = require("./routes/comment.routes");
+const historyRoutes = require("./routes/history.routes");
+const rankingRoutes = require("./routes/ranking.routes");
 
 //express app
 const app = express();
@@ -24,6 +31,13 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/accounts", accountRoutes);
+app.use("/api/novels", novelRoutes);
+app.use("/api/chapters", chapterRoutes);
+app.use("/api/bookmarks", bookmarkRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/history", historyRoutes);
+app.use("/api/ranking", rankingRoutes);
 app.get("/", (req, res) =>
     res.send("Hello, this is ebook app!")
 );
