1e7a212390ef565b419965060c2e6408e73d1e27
huyle0412 <lvdhuy0412@gmail.com>
2023-12-13 16:02:32 +0700

    update API review-rank

--- services/ranking.services.js
+++ services/ranking.services.js
@@ -3,7 +3,10 @@ const Review =require("../models/review.model");
 
 const rankingServices = {
   readCountSort: async () => {
-    const ranking = await Novel.find().sort({ readCount: -1 }).limit(5);
+    const novel = await Novel.find().sort({ readCount: -1 }).limit(5);
+    const ranking = novel.map((novel, index) => ({...novel.toObject(),
+        top: index + 1,
+    }));
     return ranking;
   },
   reviewSort: async () => {
@@ -20,16 +23,21 @@ const rankingServices = {
         },
       ]);
       const novelIds = mostReviewedNovels.slice(0,5).map((item) => item._id);
-      const novelList=[];
-      for (const id of novelIds) {
-        const novel = await Novel.findOne({_id: id});
-        if (novel) {     
-          novelList.push(novel);
-        }
-      
-      }
-
-      return novelList;
+      const novelList = await Promise.all(
+        novelIds.map(async (id, index) => {
+          const novel = await Novel.findOne({ _id: id });
+          if (novel) {
+            return { ...novel.toObject(), 
+              top: index + 1,  
+              totalReviews: mostReviewedNovels.find((item) => item._id.equals(id)).totalReviews, };
+          }
+          return null;
+        })
+      );
+  
+      const filteredNovelList = novelList.filter((novel) => novel !== null);
+  
+      return filteredNovelList;
     } catch (error) {
       throw error;
     }
